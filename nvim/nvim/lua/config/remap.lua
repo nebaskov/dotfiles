b11c8cc5a372033508b7cf7db1ad57465486ae82
nebaskov <nebaskov@tuta.io>
2024-09-15 00:33:34 +0300

    config refactoring

--- nvim/nvim/lua/config/remap.lua
+++ nvim/nvim/lua/config/remap.lua
@@ -14,7 +14,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
