af42cf273bc1a694ac44b9a86ec324e3b80f614f
nebaskov <nebaskov@tuta.io>
2024-10-03 02:05:32 +0300

    stole config from theprimeagen

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -8,9 +8,11 @@ if not vim.loop.fs_stat(lazypath) then
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
-    lazypath,
+    lazypath
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({{ import = "plugins" }})
+require("lazy").setup({
+    { import = "plugins" },
+})
