306754b173dc7d4620495e0c7a9073cfb3600229
Nikolay Shirokiy <nishirokiy@gmail.com>
2026-08-25 19:28:49 +0300

    change wezterm color scheme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,12 +7,13 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 170
-config.initial_rows = 40
+config.initial_cols = 160
+config.initial_rows = 45
 
 -- or, changing the font size and color scheme.
+-- for color schemes search https://wezterm.org/colorschemes/a/index.html
 config.font_size = 14
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Abernathy'
 
 -- Finally, return the configuration to wezterm:
 return config
